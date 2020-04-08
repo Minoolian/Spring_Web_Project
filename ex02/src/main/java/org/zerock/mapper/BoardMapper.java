a4e94cb15448ea40c0aaa1080c53493017ff5b7c
이민우 <good10045919@gmail.com>
2020-04-08 22:28:02 +0900

    ~Chapter 9.1

---
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,23 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+import org.zerock.domain.BoardVO;
+
+public interface BoardMapper {
+	
+	//@Select("select * from tbl_board where bno > 0")
+	//XML에서 SQL문이 처리되었으니 삭제
+	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
+}
