e6bc44f1608811be0e6319951a2974fe26d27cc5
이민우 <good10045919@gmail.com>
2020-05-17 21:21:23 +0900

    ~Chapter 17.4.3

---
+++ ex03/src/main/java/org/zerock/domain/BoardVO.java
@@ -0,0 +1,17 @@
+package org.zerock.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+// Lobmok을 통해 생성자, getter/setter, toString() 자동생성
+@Data
+public class BoardVO {
+	
+	private Long bno;
+	private String title;
+	private String content;
+	private String writer;
+	private Date regdate;
+	private Date updateDate;
+}
