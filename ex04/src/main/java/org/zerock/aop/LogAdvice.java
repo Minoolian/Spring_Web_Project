2dddf7b75d3f3c591118dec26095048870463e8d
이민우 <good10045919@gmail.com>
2020-05-27 22:45:36 +0900

    ~Chapter 20.1.3

--- ex04/src/main/java/org/zerock/aop/LogAdvice.java
+++ ex04/src/main/java/org/zerock/aop/LogAdvice.java
@@ -1,6 +1,10 @@
 package org.zerock.aop;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -30,5 +34,30 @@ public class LogAdvice {
 	public void logException(Exception exception) {
 		log.info("Exception...!!!!");
 		log.info("exception: "+exception);
+	}
+	
+	@Around("execution(* org.zerock.service.SampleService*.*(..))")
+	public Object logTime(ProceedingJoinPoint pjp) {
+		
+		long start=System.currentTimeMillis();
+		
+		log.info("Traget: "+pjp.getTarget());
+		log.info("Param: "+Arrays.toString(pjp.getArgs()));
+		
+		//invoke method
+		Object result=null;
+		
+		try {
+			result=pjp.proceed();
+			
+		} catch(Throwable e) {
+			e.printStackTrace();
+		}
+		
+		long end=System.currentTimeMillis();
+		
+		log.info("TIME: "+(end-start));
+		
+		return result;
 	}
 }
